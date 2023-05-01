d1fcc60c0b2a9cbfa21b404f4bc4f7ce2ddafc4e
richfjiq <rfjiq1986@gmail.com>
2023-05-01 07:41:22 -0600

    chore: simplest test - async test

--- lessons/01-write-the-simplest-test-in-javascript/index.js
+++ lessons/01-write-the-simplest-test-in-javascript/index.js
@@ -1,4 +1,4 @@
-const { sum, subtract } = require('./math');
+const { sum, subtract, sumAsync, subtractAsync } = require('./math');
 
 // console.log('Adjust math.js to pass the test.');
 // result = sum(3, 7);
@@ -29,22 +29,34 @@ function subtractTest() {
 }
 test('subtract subtracts numbers', subtractTest);
 
-function test(title, callback) {
-  try {
-    callback();
-    console.log(`✅ ${title}`);
-  } catch (error) {
-    console.error(`❌ ${title}`);
-    console.error(error);
-  }
-}
+test('sumAsync adds numbers asynchronously', async () => {
+  const result = await sumAsync(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    },
-  };
-}
+test('subtractAsync subtracts numbers asynchronously', async () => {
+  const result = await subtractAsync(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+// async function test(title, callback) {
+//   try {
+//     await callback();
+//     console.log(`✅ ${title}`);
+//   } catch (error) {
+//     console.error(`❌ ${title}`);
+//     console.error(error);
+//   }
+// }
+
+// function expect(actual) {
+//   return {
+//     toBe(expected) {
+//       if (actual !== expected) {
+//         throw new Error(`${actual} is not equal to ${expected}`);
+//       }
+//     },
+//   };
+// }
